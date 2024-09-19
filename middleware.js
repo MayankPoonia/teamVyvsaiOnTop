5ea8305c71a362f62b3043884306be2f73bd7336
ITACHI <MINATO_NAMIKAZE@MANIAC.local>
2024-09-19 23:07:43 +0530

    planCheck

--- middleware.js
+++ middleware.js
@@ -36,31 +36,22 @@ module.exports.jwtAuthenticate = (req, res, next) => {
 };
 
 module.exports.planCheck = (req, res, next) => {
-  // const token = req.cookies.UUID;
-  // if (!token) {
-  //   // console.log("No token found, redirecting to login");
-  //   return res.redirect("/login");
-  // }
-  // jwt.verify(token, process.env.JWT_AUTH_SECRET, (err, user) => {
-  //   if (err) {
-  //     // console.log("Token verification failed:", err.message);
-  //     return res.redirect("/login");
-  //   }
-  //   // console.log("Token verified, user:", user);
-  //   if (user.pln === "basic-plan" || !user.pln) {
-  //     // console.log("User plan not found, redirecting to /subscriptions");
-  //     return res.redirect("/subscriptions");
-  //   } else if (user.pln === "pro-plan" || user.plan === "enterprise-plan") {
-  //     return next();
-  //   }
-  //});
-  if (req.session.currentUser.plan) {
-    next();
-  } else {
-    req.flash(
-      "error",
-      "You Need an Active Subscription to access those resourcess"
-    );
-    res.redirect("/subscriptions");
+  const token = req.cookies.UUID;
+  if (!token) {
+    // console.log("No token found, redirecting to login");
+    return res.redirect("/login");
   }
+  jwt.verify(token, process.env.JWT_AUTH_SECRET, (err, user) => {
+    if (err) {
+      // console.log("Token verification failed:", err.message);
+      return res.redirect("/login");
+    }
+    // console.log("Token verified, user:", user);
+    if (user.pln === "basic-plan" || !user.pln) {
+      // console.log("User plan not found, redirecting to /subscriptions");
+      return res.redirect("/subscriptions");
+    } else if (user.pln === "pro-plan" || user.plan === "enterprise-plan") {
+      return next();
+    }
+  });
 };
